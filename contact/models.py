240c534016633aa6a729a2cba1ae9fbe82f1260f
rogeriokh88 <rogeriokh88@gmail.com>
2025-11-20 16:45:33 -0300

    config_admin

--- contact/models.py
+++ contact/models.py
@@ -2,19 +2,22 @@ from django.db import models
 from django.utils import timezone
 # Create your models here.
 
-#id (primary key --automático)
-#first_name(string),last_name,phone(string),
-#email(email),created_date (data),deccription(text)
+# id (primary key --automático)
+# first_name(string),last_name,phone(string),
+# email(email),created_date (data),deccription(text)
 
-#depois
-#category(foreign key),show(boolean)
-#picture(imagem),owner (foreign key)
+# depois
+# category(foreign key),show(boolean)
+# picture(imagem),owner (foreign key)
+
 
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     phone = models.CharField(max_length=50)
-    email = models.EmailField(max_length= 250)
+    email = models.EmailField(max_length=250)
     created_date = models.DateTimeField(default=timezone.now)
     decription = models.TextField(blank=True)
 
+    def __str__(self)-> str:
+        return f'{self.first_name} {self.last_name}'
