636f153b9f85fd3f42e37e86505936e4d3d4927b
rogeriokh88 <rogeriokh88@gmail.com>
2025-11-20 14:32:05 -0300

    criar banco de dado

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
+#id (primary key --automático)
+#first_name(string),last_name,phone(string),
+#email(email),created_date (data),deccription(text)
+
+#depois
+#category(foreign key),show(boolean)
+#picture(imagem),owner (foreign key)
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=50)
+    email = models.EmailField(max_length= 250)
+    created_date = models.DateTimeField(default=timezone.now)
+    decription = models.TextField(blank=True)
+
